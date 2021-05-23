9f92e4cc6365da41fe357099f63d9bf88bdb09d8
AtomArtemAnohin <takleg4e@gmail.com>
2021-05-23 17:50:41 +0300

    arrr

---
+++ backend/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { celebrate, Joi, errors } = require('celebrate');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+const { createUser, login } = require('./controllers/users');
+const { validateSignUp, validateSignIn } = require('./middlewares/validation');
+const auth = require('./middlewares/auth');
+const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.post('/signin', validateSignIn, login); // вторым аргументом передаем middleware для валидации приходящих данных до обращения к бд
+app.post('/signup', validateSignUp, createUser);
+
+app.use(auth);
+
+app.use('/', userRouter);
+app.use('/', cardRouter);
+app.use('*', () => {
+  throw new NotFoundError('Такой страницы не существует');
+});
+
+app.use(errorLogger);
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'Произошла ошибка на сервере' : message,
+  });
+  next();
+});
+app.listen(PORT);
---
+++ backend/routes/cards.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const {
+  getCards, createCard, deleteCard, likeCard, dislikeCard,
+} = require('../controllers/cards');
+const { validateCardId, validateCard } = require('../middlewares/validation');
+
+router.get('/cards', getCards);
+router.post('/cards', validateCard, createCard); // вторым аргументом передаем middleware для валидации приходящих данных до обращения к бд
+router.delete('/cards/:cardId', validateCardId, deleteCard);
+router.put('/cards/:cardId/likes', validateCardId, likeCard);
+router.delete('/cards/:cardId/likes', validateCardId, dislikeCard);
+
+module.exports = router;
